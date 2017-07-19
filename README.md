ef89762437faf09e745a63462cc6668846059abc
rodrigobotti <rodrigo.botti@gmail.com>
2017-07-19 00:32:20 -0300

    Readme + logs

---
+++ README.md
@@ -0,0 +1,7 @@
+# Usage
+- clone repo
+- `nvm use`
+- `npm install`
+- fill `input/emails.in` with an email list
+- `npm start`
+- result of the script will be at `output/emails.out`
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 const _ = require('highland')
+const chalk = require('chalk')
 
 const source = fs.createReadStream(path.join(__dirname, 'input', 'emails.in'))
 const dest = fs.createWriteStream(path.join(__dirname, 'output', 'emails.out'))
+
+const start = Date.now()
+console.log(chalk.bold.green('Process starting'))
 
 _(source)
   .split()
@@ -14,4 +18,7 @@ _(source)
   .sort()
   .collect()
   .map(list => Array.from(new Set(list)).join('\n'))
+  .errors(err => console.error(chalk.bold.red(err)))
   .pipe(dest)
+
+dest.on('close', () => console.log(chalk.bold.green(`Finished processing in ${Date.now() - start}ms`)))
